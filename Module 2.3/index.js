5b8cac582311a071274ab3dd79fd283bec6587bd
javascript 2018 <allisonsummers44@gmail.com>
2017-12-23 11:10:24 -0500

    Javascript info Module 2.3

---
+++ Module 2.3/index.js
@@ -0,0 +1,13 @@
+// 'Use strict' method
+
+"use strict";
+// this code works the modern way and must be on top
+// once use strict is used we cannot return to default
+
+// At this point in time it’s enough to know about it in general:
+
+// 1. The "use strict" directive switches the engine to the “modern” mode, changing the behavior of some built-in features. We’ll see the details as we study.
+// 2. The strict mode is enabled by "use strict" at the top. Also there are several language features like “classes” and “modules” that enable strict mode automatically.
+// 3. The strict mode is supported by all modern browsers.
+// 4. It’s always recommended to start scripts with "use strict". All examples in this tutorial assume so, unless (very rarely) specified otherwise.
+

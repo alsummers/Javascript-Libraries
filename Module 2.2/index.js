7855de7a6761d54e6211df3c427e1a09e8af4058
javascript 2018 <allisonsummers44@gmail.com>
2017-12-23 11:09:21 -0500

    Javascript info Module 2.3

---
+++ Module 2.2/index.js
@@ -0,0 +1,21 @@
+// this is practicing comment lines (ctrl+/)
+// below is code structure practice
+alert ("Say hi to this module");
+alert ("Hello");
+
+//or
+
+alert('Say hi to this module')
+alert('Hello')
+
+/* This is
+a Multiline
+comment (ctrl+shift+/) */
+
+/*
+alert("We can");
+alert("comment out");
+alert("code");
+*/
+
+

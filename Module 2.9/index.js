7af246ddf55e0de05826723216fd1ee2a28bc02f
javascript 2018 <allisonsummers44@gmail.com>
2017-12-29 19:46:02 -0500

    Javascript info Module 2.9

---
+++ Module 2.9/index.js
@@ -0,0 +1,13 @@
+alert ("Welcome");
+
+// prompts allow two variables but can have one. Also allow user to enter information
+
+let name = prompt("Enter name");
+
+alert (name);
+
+//confirm allows for true or false statements;
+
+let isCoder = confirm ("Do you know how to code?")
+
+alert (isCoder);
